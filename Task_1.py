dd50668d73b132f2554e3c57508078b65568e400
TugushevaOlga <olganika@gmail.com>
2022-02-13 14:38:18 +0300

    добавили данных в список: coomplex,byte,bytearray

--- Task_1.py
+++ Task_1.py
@@ -1,4 +1,5 @@
-my_list = [123, 1000.0, (1, 2, 3),{'key' : 'value'},{'one,two,three'},
-           frozenset('one,two,three'),False, None, 'string']
+my_list = [123, 1000.0, (5 + 5j), 'simply string', [1, 2, 3, ], (4, 5, 6),
+           {'key': 'value'}, {'one,two,three'}, frozenset('one,two,three'),
+           b'1, 2, 3',bytearray(b'1,2,3'), False, None]
 for i, item in enumerate(my_list, 1):
     print(f'{i}){item} - {type(item)}')
